a85b1e945f49418aeb7dc973489a146a70c2a190
Charlotte Smith <charliesmith5019@gmail.com>
2025-09-21 15:23:38 +0100

    add packages

--- packages.nix
+++ packages.nix
@@ -1,4 +1,24 @@
 { config, pkgs, inputs, ... }:
+let
+    nvim-fhs = (with pkgs; buildFHSEnv {
+      name = "nvf";
+      targetPkgs = pkgs: [ neovim ];
+
+      runScript = writeShellScript "nvf.sh" ''
+        exec ${neovim}/bin/nvim "$@"
+      '';
+    });
+
+    dotnet = (with pkgs.dotnetCorePackages; combinePackages [
+        sdk_8_0-bin
+        sdk_10_0-bin
+    ]);
+
+    devPackages = with pkgs; [
+        nixd
+        dotnet
+    ];
+in
 {
   users.users.charlotte.packages = with pkgs; [
     bat
@@ -13,13 +33,17 @@
     just
     keepassxc
     llvmPackages.bintools
+    networkmanagerapplet
     nodejs_24
     nushell
+    nvim-fhs
     ripgrep
     rustup
     tmux
     tree-sitter
     wofi
     zoxide
-  ];
+    unzip
+    luarocks
+  ] ++ devPackages;
 }
